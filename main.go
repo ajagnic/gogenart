94c3641850762799aa3ac000e08ae6e900628387
ajagnic <adrian.agnic@hotmail.com>
2021-02-21 17:15:30 -0500

    add cmd flags

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"image/jpeg"
 	"log"
 	"os"
@@ -8,17 +9,30 @@ import (
 	"github.com/ajagnic/go-generative-art/sketch"
 )
 
+var (
+	i   int
+	min int
+	max int
+	s   float64
+)
+
 func main() {
+	flag.IntVar(&i, "i", 10000, "number of iterations")
+	flag.IntVar(&min, "min", 3, "minimum number of polygon sides")
+	flag.IntVar(&max, "max", 5, "maximum number of polygon sides")
+	flag.Float64Var(&s, "s", 0.1, "polygon size (percentage of width)")
+	flag.Parse()
+
 	img, err := jpeg.Decode(os.Stdin)
 	if err != nil {
 		log.Fatalf("decoding error: %v", err)
 	}
 
 	canvas := sketch.NewSketch(img, sketch.Params{
-		Iterations:       10000,
-		PolygonSidesMin:  3,
-		PolygonSidesMax:  6,
-		PolygonSizeRatio: 0.1,
+		Iterations:       i,
+		PolygonSidesMin:  min,
+		PolygonSidesMax:  max,
+		PolygonSizeRatio: s,
 	})
 	canvas.Draw()
 
